e25b924bac2f85438047605f754b79e78b4edbf1
Giannis Tsagatakis <jtsagata@gmail.com>
2018-10-14 15:24:19 +0200

    & segment working (some numbers)

--- seg_driver.vhd
+++ seg_driver.vhd
@@ -11,23 +11,27 @@ begin
  DRIVER: process(BCD)
  begin
 	case BCD is
-	 when "0000" => LED_out <= "0000001"; -- "0"     
-    when "0001" => LED_out <= "1001111"; -- "1" 
-    when "0010" => LED_out <= "0010010"; -- "2" 
-    when "0011" => LED_out <= "0000110"; -- "3" 
-    when "0100" => LED_out <= "1001100"; -- "4" 
-    when "0101" => LED_out <= "0100100"; -- "5" 
-    when "0110" => LED_out <= "0100000"; -- "6" 
-    when "0111" => LED_out <= "0001111"; -- "7" 
+    --                        "GFEDCBA" 	                       
+	 -- when "0000" => LED_out <= "0000000"; -- "0"     
+    when "0001" => LED_out <= "1111001"; -- "1" 
+    when "0010" => LED_out <= "0100100"; -- "2" 
+    when "0011" => LED_out <= "0110000"; -- "3" 
+    --                        "GFEDCBA" 	                       
+	 when "0100" => LED_out <= "0011001"; -- "4" 
+    when "0101" => LED_out <= "0010010"; -- "5" 
+    -- when "0110" => LED_out <= "0100000"; -- "6" 
+    -- when "0111" => LED_out <= "0001111"; -- "7" 
     when "1000" => LED_out <= "0000000"; -- "8"     
-    when "1001" => LED_out <= "0000100"; -- "9" 
-    when "1010" => LED_out <= "0000010"; -- a
+    -- when "1001" => LED_out <= "0000100"; -- "9" 
+    -- when "1010" => LED_out <= "0000010"; -- a
 
-    when "1011" => LED_out <= "1100000"; -- b
-    when "1100" => LED_out <= "0110001"; -- C
-    when "1101" => LED_out <= "1000010"; -- d
-    when "1110" => LED_out <= "0110000"; -- E
-    when "1111" => LED_out <= "0111000"; -- F
+    -- when "1011" => LED_out <= "1100000"; -- b
+    -- when "1100" => LED_out <= "0110001"; -- C
+    -- when "1101" => LED_out <= "1000010"; -- d
+    -- when "1110" => LED_out <= "0110000"; -- E
+    -- when "1111" => LED_out <= "0111000"; -- F
+
+	 when others => LED_out <= "0000000"; -- 8
 	end case;
 end process;
 end seg_driver_impl;
--- sm_top.vhd
+++ sm_top.vhd
@@ -3,7 +3,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sm_top is
     Port ( SW : in  STD_LOGIC_VECTOR (2 downto 0); 
-           LED : out  STD_LOGIC_VECTOR (15 downto 11) );
+           LED : out  STD_LOGIC_VECTOR (15 downto 11); 
+			  SEG : out  STD_LOGIC_VECTOR (6 downto 0);
+			  AN  : out STD_LOGIC_VECTOR (7 downto 0)
+			 );
 end sm_top;
 
 architecture sm_top_impl of sm_top is
@@ -12,11 +15,25 @@ COMPONENT demux_3to5 is
     Port ( DIN : in  STD_LOGIC_VECTOR (2 downto 0);
            DOUT : out  STD_LOGIC_VECTOR (4 downto 0));
 end COMPONENT;
+
+COMPONENT seg_driver is
+    Port ( BCD : in  STD_LOGIC_VECTOR (3 downto 0);
+           LED_out : out  STD_LOGIC_VECTOR (6 downto 0));
+end COMPONENT;
+
 --- IMPORTS END ---
 
+-- SIGNALS --
+	signal BCD_in :  STD_LOGIC_VECTOR (3 downto 0);
 begin
 	
+BCD_in(3) <= '0';	
+BCD_in(2 downto 0) <= SW;	
+
+AN <= "11111110";
+	
 LEDDRIVER: demux_3to5 PORT MAP(DIN=>SW, DOUT=>LED);
+SEGDRIVER: seg_driver PORT MAP(BCD=>BCD_in, LED_out=>SEG);
 	
 end sm_top_impl;
 
