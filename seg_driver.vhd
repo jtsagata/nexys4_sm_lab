a14761e02a0046cd94a94a4517d18ca3280f9523
Kivo0 <kim0.swimmer@gmail.com>
2018-10-16 20:39:42 +0200

    config of 7 seg

--- seg_driver.vhd
+++ seg_driver.vhd
@@ -11,32 +11,37 @@ end seg_driver;
 architecture seg_driver_impl of seg_driver is
 begin
  
- AN <= "11111110";
+ AN <= "01111111";
 
  DRIVER: process(BCD)
  begin
 	case BCD is
     --                        "GFEDCBA" 	                       
-	 when "0000" => LED_out <= "0000001"; -- "0"     
+	 when "0000" => LED_out <= "1000000"; -- "0"     
     when "0001" => LED_out <= "1111001"; -- "1" 
     when "0010" => LED_out <= "0100100"; -- "2" 
     when "0011" => LED_out <= "0110000"; -- "3" 
-    --                        "GFEDCBA" 	                       
+    	                       
 	 when "0100" => LED_out <= "0011001"; -- "4" 
     when "0101" => LED_out <= "0010010"; -- "5" 
-    -- when "0110" => LED_out <= "0100000"; -- "6" 
-    -- when "0111" => LED_out <= "0001111"; -- "7" 
+    when "0110" => LED_out <= "0000010"; -- "6" 
+    when "0111" => LED_out <= "1111000"; -- "7" 
     when "1000" => LED_out <= "0000000"; -- "8"     
-    -- when "1001" => LED_out <= "0000100"; -- "9" 
-    -- when "1010" => LED_out <= "0000010"; -- a
+    when "1001" => LED_out <= "0010000"; -- "9" 
+    when "1010" => LED_out <= "0001000"; ---"a"
+--	 if(AN <= "11111110")then
+--	   AN <= "11111100";
+--		LED_out <= "1000000";
+--		--LED_out2 <= "1111001";
+--		end if; --LED_out <= "0001000"; -- a
 
-    -- when "1011" => LED_out <= "1100000"; -- b
-    -- when "1100" => LED_out <= "0110001"; -- C
-    -- when "1101" => LED_out <= "1000010"; -- d
-    -- when "1110" => LED_out <= "0110000"; -- E
-    -- when "1111" => LED_out <= "0111000"; -- F
+    when "1011" => LED_out <= "0000011"; -- b
+    when "1100" => LED_out <= "1000110"; -- C
+    when "1101" => LED_out <= "0100001"; -- d
+    when "1110" => LED_out <= "0000110"; -- E
+    when "1111" => LED_out <= "0001110"; -- F
 
-	 when others => LED_out <= "0000000"; -- 8
+	 when others => LED_out <= "1111111"; -- (no number<= leds off)
 	end case;
 end process;
 end seg_driver_impl;
